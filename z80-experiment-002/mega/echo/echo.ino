dd38604d68639df64f9aa52573710c6edbfae237
Stephen Willcock <stephen.willcock@gmail.com>
2024-03-26 09:29:15 +0000

    Multiple changes

--- z80-experiment-002/mega/echo/echo.ino
+++ z80-experiment-002/mega/echo/echo.ino
@@ -1,47 +1,9 @@
-// Define constants for the Z80 pins and the Arduino Mega pins to which they are connected
+#include "z80.h"
+#include "mem.h"
 
-// Address Bus
-const char A[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37}; // Z80 30-40,1-5 Output
-
-// Data Bus
-const char D[] = {38,39,40,41,42,43,44,45}; // Z80 14,15,12,8,7,9,10,13 Input/Output
-
-// System Control
-const char M1 =     46; // Z80 27 Output
-const char MREQ =   47; // Z80 19 Output
-const char IORQ =  48; // Z80 20 Output
-const char RFSH =   49; // Z80 28 Output
-
-// System Control - used for interrupts
-const char RD =     18; // Z80 21 Output
-const char WR =     19; // Z80 22 Output
-
-// CPU Control
-const char HALT =   50; // Z80 18 Output
-const char WAIT =   51; // Z80 24 Input
-// const char INT =     4; // Z80 16 Input // not currently needed but planning out the pins
-// const char NMI =     5; // Z80 17 Input // not currently needed but planning out the pins
-const char RESET =   6; // Z80 26 Input
-
-// CPU Bus Control
-// const char BUSRQ =   7; // Z80 25 Input // not currently needed but planning out the pins
-// const char BUSACK =  8; // Z80 23 Output // not currently needed but planning out the pins
-
-// Clock
-// Note that we are using the clock to trigger an interrupt. Arduino Mega will handle interrupts only on 2, 3, 18, 19, 20, 21 
-// and 20-21 have special considerations - https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-const char CLK =     2; // Z80 6  Input
-
-// Mock Memory
-// 1     0000              start:
-// 2     0000 DB 00            in a, 0
-// 3     0002 D3 00            out 0, a
-// 4     0004 C3 00 00         jp start
-// 5     0007
-byte MEM[] = { 
-0xdb, 0x00, 0xd3, 0x00, 0xc3, 0x00, 0x00
-}; 
 const unsigned int mem_size = sizeof(MEM);
+const unsigned int SER_CTR = 0;
+const unsigned int SER_DATA = 1;
 
 // internal Arduino state 
 String cache = "";
@@ -66,31 +28,37 @@ void loop() { // loop is needed only to flush the message cache - the work is do
 } 
 
 void readInterrupt() {
-  writeLogBusStatus();
   if(digitalRead(RD)==LOW) { // we are here because RD has changed - if RD is LOW then it has just become LOW - a falling edge and so the beginning of a read
     for(int i=0; i<8; i++) pinMode(D[i], OUTPUT); // switch the data pins to output mode
     unsigned int a=0;
-    for(int i=0; i<16; i++) a = bitWrite(a,i,digitalRead(A[i])); // get the address as a number
     byte d = 0x00;
     if(digitalRead(MREQ)==LOW) { // this is a read of memory
+      for(int i=0; i<16; i++) bitWrite(a,i,digitalRead(A[i])); // get the address as a number
       if(a<mem_size) { 
         d = MEM[a];
       }
     }
     if(digitalRead(IORQ)==LOW) { // read of IO - do we need to check this? is it not implied?
-      int i = Serial.read();
-      d = i < 0 ? 0 : i;
+      for(int i=0; i<8; i++) bitWrite(a,i,digitalRead(A[i])); // get the address as a number from the bottom 8 bits
+      if(a==SER_CTR) { // simulated control register - all we want to return is whether there is data in the buffer
+        bitWrite(d,0,(Serial.available()>0));
+      }
+      if(a==SER_DATA) {
+        int i = Serial.read();
+        d = i < 0 ? 0 : i;
+      }
     }
     for(int i=0; i<8; i++) digitalWrite(D[i],bitRead(d,i)); // put the data from memory on the bus, bit by bit
+    writeLogBusStatus();
+    writeLogLn();
   }
   else { // RD is HIGH and has just become HIGH - rising edge and so the end of the read process
     for(int i=0; i<8; i++) pinMode(D[i], INPUT); // switch the pins back to input mode
   }
-  writeLogLn();
 }
 
 void writeInterrupt() {
-  writeLogBusStatus();
+  String s;
   if(digitalRead(WR)==LOW) { // we are here because WR has changed - if WR is LOW then it has just become LOW - a falling edge and so the beginning of a write
     for(int i=0; i<8; i++) pinMode(D[i], INPUT);  // switch the data pins to input mode
     isIORQ = (digitalRead(IORQ)==LOW);            // set the flag that we're writing to an IO - will capture on the rising edge
@@ -98,26 +66,26 @@ void writeInterrupt() {
   else { // WR rising edge - this is when we will capture the data that the Z80 has put on the bus
     if(isIORQ) {
       byte d = 0;
-      for(int i=0; i<8; i++) d = bitWrite(d,i,digitalRead(D[i]));
-      writeOut(String((char)d));
+      for(int i=0; i<8; i++) bitWrite(d,i,digitalRead(D[i]));
+      s = String((char)d);
     }
     else {
       unsigned int a = 0;
-      for(int i=0; i<16; i++) a = bitWrite(a,i,digitalRead(A[i]));
+      for(int i=0; i<16; i++) bitWrite(a,i,digitalRead(A[i]));
       if(a<mem_size) {
         byte d = 0;
-        for(int i=0; i<8; i++) d = bitWrite(d,i,digitalRead(D[i]));
+        for(int i=0; i<8; i++) bitWrite(d,i,digitalRead(D[i]));
         MEM[a] = d;
       }
     }
+    writeLogBusStatus();
+    writeOut(s);
+    writeLogLn();
   }
-  writeLogLn();
 }
 
 void writeLogBusStatus() {
   if(LOGGING) {
-    writeLog(digitalRead(CLK)==LOW ? "CLK:0" : "CLK:1");
-    writeLog(digitalRead(RFSH)==LOW ? "RFSH" : "....");
     writeLog(digitalRead(M1)==LOW   ? "M1" : "..");
     writeLog(digitalRead(RD)==LOW   ? "RD" : "..");
     writeLog(digitalRead(WR)==LOW   ? "WR" : "..");
@@ -125,13 +93,13 @@ void writeLogBusStatus() {
     writeLog(digitalRead(IORQ)==LOW ? "IORQ" : "....");
     // build a number from the address bits
     unsigned int a = 0;
-    for(int i=0; i<16; i++) a = bitWrite(a,i,digitalRead(A[i]));
+    for(int i=0; i<16; i++) bitWrite(a,i,digitalRead(A[i]));
     char s[20];
     sprintf(s,"A:%04x",a);
     writeLog(s);
     // build a number from the data bits
     byte d = 0;
-    for(int i=0; i<8; i++) d = bitWrite(d,i,digitalRead(D[i]));
+    for(int i=0; i<8; i++) bitWrite(d,i,digitalRead(D[i]));
     sprintf(s,"D:%02x",d);
     writeLog(s);
     writeLog("|");
